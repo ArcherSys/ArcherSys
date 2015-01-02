67adc200852310d35edd3c9e08c25477a35dad8a
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2015-01-02 16:47:24 -0500

    New YiiTube

--- entertainment/yiitube/index.php
+++ entertainment/yiitube/index.php
@@ -8,13 +8,19 @@
 <div class="yiitube-container">
   <script type="text/x-handlebars" data-template-name="channels">
   <header><h1>YiiTube ASOS Streaming</h1></header>
- <div class="yiitube-content">{{outlet}}</div>
+ <div class="yiitube-content">{{#each in channel itemController="channel"}}
+  <h2>{{name}}</h2>
+{{/each}}</div>
 </div>
 </script>
+
 
 <script src="js/libs/jquery-1.10.2.js"></script>
 <script src="js/libs/handlebars-v2.0.0.js"></script>
 <script src="js/libs/ember-1.9.1.js"></script>
+<script src="js/libs/ember-data.min.js"></script>
 <script src="js/app.js"></script>
+<script src="js/model/channel.js"></script>
+<script src="js/controllers/channel_controller.js"></script>
 </body>
 </html>
