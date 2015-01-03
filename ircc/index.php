f3aba729a0f2945348b3ebf0a70ff7226a5acca4
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2015-01-02 21:51:39 -0500

    Viewer

---
+++ ircc/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<script src="/core/js/react-0.11.2/build/react.js"></script>
+<script src="/core/js/react-0.11.2/build/JSXTransformer.js"></script>
+<script src="/core/js/jquery.js"></script>
+<script type="text/jsx" src="/core/js/components/IRC.js"></script>
+<script type="text/jsx">
+React.render(<ArcherSysIRCClient />,$(".archersys-irc"));</script>
+</script>
+</head>
+<body>
+<div class="archersys-irc"></div>
+</body>
+</html>
