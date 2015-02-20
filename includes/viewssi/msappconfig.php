eee0aa9425247006b4e56fac1bd2fa0f4f34bf63
unknown <Student@458S-G76T9Y1.students.dc_schools.k12.dc.us>
2015-02-20 13:30:11 -0500

    New

--- includes/viewssi/msappconfig.php
+++ includes/viewssi/msappconfig.php
@@ -6,3 +6,4 @@
 <meta name="msapplication-navbutton-color" content="#3480C0" />
 <meta name="msapplication-window" content="width=1024;height=768" />
 <meta name="msapplication-tooltip" content="Start ArcherSys OS" />
+<meta name="msapplication-badge" content="frequency=30;pollinguri=http://localhost:80">
