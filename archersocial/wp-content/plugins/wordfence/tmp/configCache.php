0fa3bb39ccd7dec3085158751634c77f2673d24d
unknown <Student@458S-G76T9Y1.students.dc_schools.k12.dc.us>
2015-03-02 10:41:17 -0500

    RT3.0

---
+++ archersocial/wp-content/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:17:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"1";s:6:"apiKey";s:160:"a40ff0f54c3a5ceaaf48f142a762ab0816bbf5fcacf9bb98e8fa843de489347d2202a2146c685a4f8e88c7d8f53b21f1bfd62662384a1b076d5a3451a84451990a752274335efe850fb98c18519073b7";s:9:"howGetIPs";s:0:"";s:11:"whitelisted";s:0:"";s:13:"cbl_countries";s:0:"";s:18:"cbl_bypassRedirURL";s:0:"";s:17:"cbl_bypassViewURL";s:0:"";s:11:"blockedTime";s:3:"300";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:10:"tourClosed";s:1:"1";s:10:"autoUpdate";s:1:"1";s:11:"alertEmails";s:0:"";s:17:"actUpdateInterval";s:0:"";s:7:"debugOn";s:1:"0";s:13:"welcomeClosed";s:0:"";}
