2e8b26eb30ca641d269c25b5bfb615a42d208b30
Weldon Henson <weldon23.henson23@gmail.com>
2015-03-10 10:04:48 -0400

    New

--- archersysoscashew/settings.py
+++ archersysoscashew/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+  'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
      'registration',
      'oauth2_provider',
+     
 
 )
 
@@ -87,3 +89,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'c:\xampp\htdocs\archersysoscashew\static\'
