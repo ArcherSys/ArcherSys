57036ca663c24fcfe96bca4ea2380285f6f8f9af
Weldon Henson <weldon23.henson23@gmail.com>
2015-03-20 15:13:41 -0400

    New:

--- archerosapp.py
+++ archerosapp.py
@@ -1,13 +1,19 @@
-import configparser, http.server
+import configparser, http.server, os
+from pathlib import Path
 class ArcherSysApplication(object):
    def __init__(self, file):
+     
      self.config= configparser.ConfigParser()
      self.file = file
      self.server = None
    
-   def run(self,app,server_class=http.server.HTTPServer, handler_class=http.server.BaseHTTPRequestHandler):
-       self.config.read(self.file)
-       self.config.sections()
-       server_address =  (self.config[app]["IP"],int(self.config[app]["Port"]))
-       httpd = server_class(server_address, handler_class)
-       httpd.serve_forever()
+   def run(self,app,server_class=http.server.HTTPServer, handler_class=http.server.SimpleHTTPRequestHandler):
+     self.js = Path('\js')
+	 if self.js.exists() == False:
+         os.system("git pull origin master")	
+	 else:  
+	     self.config.read(self.file)
+         self.config.sections()
+         server_address =  (self.config[app]["IP"],int(self.config[app]["Port"]))
+         httpd = server_class(server_address, handler_class)
+         httpd.serve_forever()
---
+++ docs-python/Scripts/pybabel-script.py
@@ -0,0 +1,10 @@
+#!C:\xampp\htdocs\docs-python\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Babel==1.3','console_scripts','pybabel'
+__requires__ = 'Babel==1.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Babel==1.3', 'console_scripts', 'pybabel')()
+    )
