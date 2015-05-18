1fe782f893aeff1188b44d2039df3f7dc2e1b602
Wedon Henson <acosf2014.pdflint@gmail.com>
2015-05-18 11:32:25 -0400

    New

---
+++ Lib/archersys/apps/coreturtles.py
@@ -0,0 +1,18 @@
+import turtle
+'''
+Created on May 18, 2015
+
+@author: Weldon Henson
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
