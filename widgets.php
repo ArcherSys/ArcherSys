964d267bfbb4f3173d65c2c2b98b185d3456daa6
Weldon Henson <acosf2014.pdflint@gmail.com>
2015-11-25 08:39:47 -0500

    Widgets Page

---
+++ widgets.php
@@ -0,0 +1,38 @@
+<html>
+	
+<head>
+	
+	<link rel="stylesheet" href="/core/js/flipclock/compiled/flipclock.css">
+
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+</script>
+
+	
+	<script src="/core/js/compiled/flipclock.js"></script>		
+
+	</head>
+	<body>
+		
+<div class="clock" style="margin:2em;"></div>
+
+	
+	<script type="text/javascript">
+		
+	var clock;
+			
+			
+$(document).ready(function() {
+			
+	clock = $('.clock').FlipClock({
+			
+		clockFace: 'TwelveHourClock'
+			
+	});
+			});
+
+		</script>
+	
+	
+	</body>
+</html>
