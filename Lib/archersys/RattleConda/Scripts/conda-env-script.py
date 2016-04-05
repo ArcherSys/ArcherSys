ca1dc2722588584662db5733e317e046d075305b
unknown <9082117@CTE458S-91ypq22.students.ad.k12.dc.us>
2016-04-05 13:51:36 -0400

    Fix Py

--- Lib/archersys/RattleConda/Scripts/conda-env-script.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    import sys
-    from conda_env.cli.main import main
-
-    sys.exit(main())
--- Lib/archersys/RattleConda/Scripts/conda-script.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    import sys
-    from conda.cli.main import main
-
-    sys.exit(main())
